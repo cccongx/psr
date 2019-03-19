6f426dd4ca6a86fef03ce3672cf639c6d5e65887
Cleverina Cong <cccongx@gmail.com>
2019-03-18 21:20:39 -0400

    Add files via upload

---
+++ header.php
@@ -0,0 +1,42 @@
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="https://phisigmarho.org/sites/default/files/PSR-logo-icon-round-small_0.ico" type="image/vnd.microsoft.icon">
+
+</head>
+
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light static-top">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <img src="coat.jpg" width="35px">
+        PHI SIGMA RHO
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home
+                <span class="sr-only">(current)</span>
+              </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="about.html">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="rush.html">Rush</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="sisters.html">Sisters</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="house.html">House</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
---
+++ testindex.php
@@ -0,0 +1,8 @@
+<?php $title = 'Home'; ?>
+<?php $metaTags = 'tag1 tag2'; ?>
+<?php $currentPage = 'index'; ?>
+<?php include('index.html'); ?>
+<?php include('navbar.php'); ?>
+<body>
+Index Page, Home has an active class
+<?php include('footer.php'); ?>
